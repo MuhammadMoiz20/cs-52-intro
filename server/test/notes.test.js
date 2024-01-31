5f0f35195f4aa31f265547b4772f3582990fa80d
Muhammad Moiz <muhammad.moiz.26@dartmouth.edu>
2024-01-30 23:42:58 -0500

    wip: server test setup

---
+++ server/test/notes.test.js
@@ -0,0 +1,10 @@
+// note: these are pretty basic, just smoke tests
+const assert = require('assert');
+const request = require('supertest');
+// would normally start app here, skip for now
+
+describe('notes', () => {
+  it('placeholder', () => {
+    assert.strictEqual(1 + 1, 2);
+  });
+});
