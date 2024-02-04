5b31405fede04b6a21efc5a5715ae85e416ed3f4
Muhammad Moiz <muhammad.moiz.26@dartmouth.edu>
2024-02-03 20:34:29 -0500

    fetch and show notes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './api';
 
 export default function App() {
-  return <h1>notes</h1>;
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    api.get('/notes').then(r => setNotes(r.data)).catch(() => {});
+  }, []);
+  return (
+    <div>
+      <h1>notes</h1>
+      <ul>
+        {notes.map(n => <li key={n._id}>{n.text}</li>)}
+      </ul>
+    </div>
+  );
 }
