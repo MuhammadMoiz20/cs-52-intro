c6483fff5bccbdf13f1254da5a0c11061572a07b
Muhammad Moiz <muhammad.moiz.26@dartmouth.edu>
2024-02-10 20:58:15 -0500

    use env var in client api + clear token on 401

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:4000' });
+const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+const api = axios.create({ baseURL });
 
 api.interceptors.request.use((config) => {
   const t = localStorage.getItem('token');
   if (t) config.headers.Authorization = 'Bearer ' + t;
   return config;
+});
+
+api.interceptors.response.use(r => r, (err) => {
+  if (err.response && err.response.status === 401) {
+    localStorage.removeItem('token');
+  }
+  return Promise.reject(err);
 });
 
 export default api;
